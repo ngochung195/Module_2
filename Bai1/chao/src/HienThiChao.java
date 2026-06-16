2950edaa19dc36b1e457debe0c689b05df477211
Nguyen Ngoc Hung <nghung2k4@gmail.com>
2026-06-16 14:32:00 +0700

    chao

---
+++ Bai1/chao/src/HienThiChao.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class HienThiChao {
+    public static void main(String[] args) {
+        System.out.println("Nhập tên người dùng: ");
+
+        String name = scanner.nextLine();
+
+        System.out.println("Xin chào: " + name);
+    }
+}
