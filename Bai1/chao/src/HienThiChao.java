fdf9fb39a1a6914e71d5620476bdca5d4311248e
Nguyen Ngoc Hung <nghung2k4@gmail.com>
2026-06-16 14:34:31 +0700

    update

--- Bai1/chao/src/HienThiChao.java
+++ Bai1/chao/src/HienThiChao.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class HienThiChao {
     public static void main(String[] args) {
         System.out.println("Nhập tên người dùng: ");
+        Scanner scanner = new Scanner(System.in);
 
         String name = scanner.nextLine();
 
