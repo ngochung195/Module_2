c082f25269fe4ad88513327afe5dacb9ce7b9edf
Nguyen Ngoc Hung <nghung2k4@gmail.com>
2026-06-23 11:42:57 +0700

    Comparable Circle

---
+++ Bai7/interface_comparable/src/ComparableCircleTest.java
@@ -0,0 +1,76 @@
+import java.util.Arrays;
+
+class Circle {
+    private double radius = 1.0;
+    private String color = "blue";
+    private boolean filled = true;
+
+    public Circle() {
+    }
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    public Circle(double radius, String color, boolean filled) {
+        this.radius = radius;
+        this.color = color;
+        this.filled = filled;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    @Override
+    public String toString() {
+        return "Circle with radius = " + this.radius + ", which is a subclasss of A Shape with color of "
+                + this.color + " and " + (filled ? "filled" : "not filled");
+    }
+}
+
+class ComparableCircle extends Circle
+        implements Comparable<ComparableCircle> {
+
+    public ComparableCircle() {
+    }
+
+    public ComparableCircle(double radius) {
+        super(radius);
+    }
+
+    public ComparableCircle(double radius, String color, boolean filled) {
+        super(radius, color, filled);
+    }
+
+    @Override
+    public int compareTo(ComparableCircle o) {
+        if (getRadius() > o.getRadius())
+            return 1;
+        else if (getRadius() < o.getRadius())
+            return -1;
+        else
+            return 0;
+    }
+}
+
+public class ComparableCircleTest {
+    public static void main(String[] args) {
+        ComparableCircle[] circles = new ComparableCircle[3];
+        circles[0] = new ComparableCircle(3.6);
+        circles[1] = new ComparableCircle();
+        circles[2] = new ComparableCircle(3.5, "indigo", false);
+
+        System.out.println("Pre-sorted:");
+        for (ComparableCircle circle : circles) {
+            System.out.println(circle);
+        }
+
+        Arrays.sort(circles);
+
+        System.out.println("After-sorted:");
+        for (ComparableCircle circle : circles) {
+            System.out.println(circle);
+        }
+    }
+}
