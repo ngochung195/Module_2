1561787c3940ea5874c521b95d90c4d7446d3612
Nguyen Ngoc Hung <nghung2k4@gmail.com>
2026-07-06 10:30:25 +0700

    gtln file

---
+++ Bai16/gtln_file/src/ReadAndWriteFile.java
@@ -0,0 +1,61 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ReadAndWriteFile {
+    public List<Integer> readFile(String filePath) {
+        List<Integer> numbers = new ArrayList<>();
+        try {
+            File file = new File(filePath);
+            if (!file.exists()) {
+                throw new FileNotFoundException();
+            }
+
+            BufferedReader bufferedReader = new BufferedReader(new FileReader(file));
+            String line = "";
+
+            while ((line = bufferedReader.readLine()) != null) {
+                numbers.add(Integer.parseInt(line));
+            }
+
+            bufferedReader.close();
+        } catch (Exception e) {
+            System.out.println("File khong ton tai hoac noi dung co loi!");
+        }
+        return numbers;
+    }
+
+    public void writeFile(String filePath, int max) {
+        try {
+            FileWriter writer = new FileWriter(filePath, true);
+            BufferedWriter bufferedWriter = new BufferedWriter(writer);
+            bufferedWriter.write("Gia tri lon nhat la: " + max);
+            bufferedWriter.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static int findMax(List<Integer> numbers) {
+        int max = numbers.get(0);
+        for (int i = 1; i < numbers.size(); i++) {
+            if (max < numbers.get(i)) {
+                max = numbers.get(i);
+            }
+        }
+        return max;
+    }
+
+    public static void main(String[] args) {
+        ReadAndWriteFile readAndWriteFile = new ReadAndWriteFile();
+        List<Integer> numbers = readAndWriteFile.readFile("numbers.txt");
+        int maxValue = findMax(numbers);
+        readAndWriteFile.writeFile("result.txt", maxValue);
+    }
+}
