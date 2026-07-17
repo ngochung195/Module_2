e99c9127f9a46a6e822e931596c6e240a0331c6f
Nguyen Ngoc Hung <nghung2k4@gmail.com>
2026-07-17 16:09:13 +0700

    meal

---
+++ Bai23/ template_method_meal/src/TemplateMethodDemo.java
@@ -0,0 +1,72 @@
+abstract class Meal {
+    // template method
+    public final void doMeal() {
+        prepareIngredients();
+        cook();
+        eat();
+        cleanUp();
+    }
+
+    public abstract void prepareIngredients();
+
+    public abstract void cook();
+
+    public void eat() {
+        System.out.println("Mmm, that's good");
+    }
+
+    public abstract void cleanUp();
+
+}
+
+class HamburgerMeal extends Meal {
+
+    @Override
+    public void prepareIngredients() {
+        System.out.println("Getting burgers, buns, and french fries");
+    }
+
+    @Override
+    public void cook() {
+        System.out.println("Cooking burgers on grill and fries in oven");
+    }
+
+    @Override
+    public void cleanUp() {
+        System.out.println("Throwing away paper plates");
+    }
+}
+
+class TacoMeal extends Meal {
+    @Override
+    public void prepareIngredients() {
+        System.out.println("Getting ground beef and shells");
+    }
+
+    @Override
+    public void cook() {
+        System.out.println("Cooking ground beef in pan");
+    }
+
+    @Override
+    public void eat() {
+        System.out.println("The tacos are tasty");
+    }
+
+    @Override
+    public void cleanUp() {
+        System.out.println("Doing the dishes");
+    }
+}
+
+public class TemplateMethodDemo {
+    public static void main(String[] args) {
+        Meal meal1 = new HamburgerMeal();
+        meal1.doMeal();
+
+        System.out.println();
+
+        Meal meal2 = new TacoMeal();
+        meal2.doMeal();
+    }
+}
